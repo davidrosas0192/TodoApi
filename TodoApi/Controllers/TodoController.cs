15112d47f261d218a7465e626edbb365ab7e0449
David de la Cruz <Rey.Cruz@alere.com>
2020-11-18 17:13:27 -0800

    Define API controller class without  methods

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _todoContext;
+
+        public TodoController(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+
+            if(_todoContext.TodoItems.Count() == 0)
+            {
+                //create a new todoItem if collection is empty,
+                //wich means you can't delete al todoItems
+                _todoContext.TodoItems.Add(new TodoItem { Name = "item1" });
+                _todoContext.SaveChanges();
+            }
+        }
+    }
+}
