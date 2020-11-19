81363a115f048cb75fa3395e7f8d86ebdd411366
David de la Cruz <Rey.Cruz@alere.com>
2020-11-18 17:38:18 -0800

    Add methods get on controller todo + change property in todo model todo class

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,19 +13,36 @@ namespace TodoApi.Controllers
     [Route("api/[controller]")]
     public class TodoController : ControllerBase
     {
-        private readonly TodoContext _todoContext;
+        private readonly TodoContext _context;
 
         public TodoController(TodoContext todoContext)
         {
-            _todoContext = todoContext;
+            _context = todoContext;
 
-            if(_todoContext.TodoItems.Count() == 0)
+            if(_context.TodoItems.Count() == 0)
             {
                 //create a new todoItem if collection is empty,
                 //wich means you can't delete al todoItems
-                _todoContext.TodoItems.Add(new TodoItem { Name = "item1" });
-                _todoContext.SaveChanges();
+                _context.TodoItems.Add(new TodoItem { Name = "item1" });
+                _context.SaveChanges();
             }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoITems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
         }
     }
 }
--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -7,7 +7,7 @@ namespace TodoApi.Models
 {
     public class TodoItem
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
 
         public string Name { get; set; }
 
